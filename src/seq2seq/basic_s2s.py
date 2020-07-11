8a508a3c2f42dea79619c557e3fed99774b0ddd4
Mateusz Bednarski <mateusz.bednarski@windowslive.com>
2020-07-11 18:29:41 +0200

    Initial s2s model

--- src/seq2seq/basic_s2s.py
+++ src/seq2seq/basic_s2s.py
@@ -1,12 +1,21 @@
-class SimpleSeq2Seq(pl.LightningModule):
-    def __init__(self, context_size=128, embedding_size=24):
-        super(SimpleSeq2Seq, self).__init__()
+import torch
+import torch.nn as nn
+import pytorch_lightning as pl
+from seq2seq.dataset.math_dataset import MathDataset
+from torch.utils.data.dataloader import DataLoader
 
+
+class SimpleSeq2Seq(pl.LightningModule):
+    def __init__(self):
+        super(SimpleSeq2Seq, self).__init__()
+        context_size = 256
+        embedding_size = 64
         # embedding
-        self.embed = nn.Embedding(
-            num_embeddings=tokenizer.get_vocab_size(),
-            embedding_dim=embedding_size,
-            padding_idx=0,
+        self.num_embed = nn.Embedding(
+            num_embeddings=17, embedding_dim=embedding_size, padding_idx=0,
+        )
+        self.text_embed = nn.Embedding(
+            num_embeddings=39, embedding_dim=embedding_size, padding_idx=0,
         )
 
         # encoder
@@ -18,7 +27,7 @@ class SimpleSeq2Seq(pl.LightningModule):
         self.decoder_rnn = nn.GRU(
             input_size=embedding_size, hidden_size=context_size, batch_first=True
         )
-        self.fc = nn.Linear(context_size, tokenizer.get_vocab_size())
+        self.fc = nn.Linear(context_size, 39)
 
         # loss
         self.criterion = nn.CrossEntropyLoss(ignore_index=0)
@@ -39,7 +48,7 @@ class SimpleSeq2Seq(pl.LightningModule):
         # print('max_seq_len', max_seq_len)
 
         # encode
-        embedded = self.embed(x)
+        embedded = self.num_embed(x)
         # print('embedded', embedded.shape)
 
         # embedded: BxLxE
@@ -60,7 +69,7 @@ class SimpleSeq2Seq(pl.LightningModule):
         for i in range(max_seq_len - 1):
 
             # embedded_input: Bx1xE
-            embedded_input = self.embed(decoder_input)
+            embedded_input = self.text_embed(decoder_input)
             # print('embedded_input', embedded_input.shape)
 
             # after_rnn
@@ -89,24 +98,24 @@ class SimpleSeq2Seq(pl.LightningModule):
 
         return {"loss": loss}
 
-    def validation_step(self, batch, batch_idx):
+    def prediction(self, batch, batch_idx):
         x, y = batch
 
         batch_size = x.shape[0]
         max_seq_len = x.shape[1]
 
-        embedded = self.embed(x)
+        embedded = self.num_embed(x)
 
         _, context_vector = self.encoder_rnn(embedded)
 
-        decoder_input = (
-            torch.Tensor(batch_size * [1]).unsqueeze(1).long().cuda()
-        )  # cuda?
+        decoder_input = torch.Tensor(batch_size * [1]).unsqueeze(1).long()  # cuda?
 
         loss = 0
 
+        dec_outputs = []
+
         for i in range(max_seq_len - 1):
-            embedded_input = self.embed(decoder_input)
+            embedded_input = self.text_embed(decoder_input)
 
             after_rnn_out, context_vector = self.decoder_rnn(
                 embedded_input, context_vector
@@ -119,20 +128,22 @@ class SimpleSeq2Seq(pl.LightningModule):
                 torch.argmax(decoder_output, dim=1).unsqueeze(1).detach()
             )  # MB: how about teacher forcing?
 
+            dec_outputs.append(pred_idx)
+
             decoder_input = pred_idx
 
             loss += self.criterion(decoder_output, y[:, i])
 
         loss = loss / max_seq_len
 
-        return {"loss": loss}
+        return dec_outputs
 
-    def validation_epoch_end(self, outputs):
-        losses = torch.stack([x["loss"] for x in outputs])
+    # def validation_epoch_end(self, outputs):
+    #     losses = torch.stack([x["loss"] for x in outputs])
 
-        mean_loss = torch.mean(losses)
-        print(mean_loss)
-        return {"log": {"mean_val_loss": mean_loss}}
+    #     mean_loss = torch.mean(losses)
+    #     print(mean_loss)
+    #     return {"log": {"mean_val_loss": mean_loss}}
 
     def forward(self, x):
         return x
@@ -141,26 +152,27 @@ class SimpleSeq2Seq(pl.LightningModule):
         return torch.optim.Adam(self.parameters())
 
     def train_dataloader(self):
-        reversed_dataset = ReverseDataset(train_samples, tokenizer)
+        dset = MathDataset()
         train_loader = DataLoader(
-            reversed_dataset,
-            batch_size=16,
-            shuffle=True,
-            collate_fn=tokenizer.collate_sequences,
+            dset, batch_size=16, shuffle=True, collate_fn=dset.collate,
         )
         return train_loader
 
-    def val_dataloader(self):
-        reversed_dataset = ReverseDataset(val_samples, tokenizer)
-        loader = DataLoader(
-            reversed_dataset,
-            batch_size=16,
-            shuffle=False,
-            collate_fn=tokenizer.collate_sequences,
-        )
-        return loader
+    # def val_dataloader(self):
+    #     reversed_dataset = ReverseDataset(val_samples, tokenizer)
+    #     loader = DataLoader(
+    #         reversed_dataset,
+    #         batch_size=16,
+    #         shuffle=False,
+    #         collate_fn=tokenizer.collate_sequences,
+    #     )
+    #     return loader
 
 
-model = SimpleSeq2Seq()
-trainer = pl.Trainer(gpus=1, gradient_clip=1.0, max_epochs=10, fast_dev_run=False)
-trainer.fit(model)
+if __name__ == "__main__":
+
+    model = SimpleSeq2Seq()
+    trainer = pl.Trainer(
+        gpus=[0], gradient_clip_val=1.0, max_epochs=100, fast_dev_run=False,
+    )
+    trainer.fit(model)
---
+++ src/seq2seq/dataset/math_dataset.py
@@ -0,0 +1,44 @@
+from torch.utils.data import Dataset
+from torch.nn.utils.rnn import pad_sequence
+from seq2seq.tokenizers import NumberTokenizer, TextTokenizer
+
+import pandas as pd
+
+
+class MathDataset(Dataset):
+    def __init__(self):
+        super().__init__()
+        self.df = pd.read_csv("data/raw/math.csv")
+
+        self.number_tokenizer = NumberTokenizer()
+        self.text_tokenizer = TextTokenizer(self.df["text"])
+
+    def __len__(self):
+        return len(self.df)
+
+    def __getitem__(self, idx):
+        x = self.df["numbers"][idx]
+        y = self.df["text"][idx]
+
+        x = self.number_tokenizer.encode(x, add_special_tokens=True)
+        y = self.text_tokenizer.encode(y, add_special_tokens=True)
+
+        return x, y
+
+    def collate(self, batch):
+        x, y = zip(*batch)
+
+        x = list(x)
+        y = list(y)
+
+        x = pad_sequence(x, batch_first=True, padding_value=0)
+        y = pad_sequence(y, batch_first=True, padding_value=0)
+
+        return x, y
+
+
+if __name__ == "__main__":
+    md = MathDataset()
+    print(md[0])
+    print(md.number_tokenizer.tok2idx)
+    print(md.text_tokenizer.tok2idx)
