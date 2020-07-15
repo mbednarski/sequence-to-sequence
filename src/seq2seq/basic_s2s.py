a0672fee26032bd5c88dec9f69807c2f2cdb4c12
Mateusz Bednarski <mateusz.bednarski@windowslive.com>
2020-07-15 13:58:33 +0200

    Generating train/val/test

--- src/seq2seq/basic_s2s.py
+++ src/seq2seq/basic_s2s.py
@@ -128,48 +128,7 @@ class SimpleSeq2Seq(pl.LightningModule):
 
         return {"loss": loss}
 
-        # # context_vector: BxC ! LSTM 2xC
-        # # print('context_vector shape', context_vector.shape)
-
-        # # decoder_input: Bx1
-        # decoder_input = torch.Tensor(batch_size * [1]).unsqueeze(1).long()
-        # # cuda?
-        # # print('decoder_input shape', decoder_input.shape)
-        # # print('decoder_input', decoder_input)
-
-        # loss = 0
-
-        # for i in range(max_seq_len - 1):
-
-        #     # embedded_input: Bx1xE
-        #     embedded_input = self.text_embed(decoder_input)
-        #     # print('embedded_input', embedded_input.shape)
-
-        #     # after_rnn
-        #     after_rnn_out, context_vector = self.decoder_rnn(
-        #         embedded_input, context_vector
-        #     )
-
-        #     # print('after_rnn_out shape' ,after_rnn_out.shape )
-        #     # print('after_rnn_out' ,after_rnn_out)
-        #     # print('context_vector shape' ,context_vector.shape )
-        #     # print('context_vector' ,context_vector)
-
-        #     # decoder_output
-        #     decoder_output = self.fc(after_rnn_out.squeeze())
-        #     # print('decoder_output shape' , decoder_output.shape)
-
-        #     pred_idx = torch.argmax(decoder_output, dim=1).unsqueeze(1).detach()
-
-        #     # print('pred_idx shape', pred_idx.shape)
-
-        #     decoder_input = pred_idx
-
-        #     loss += self.criterion(decoder_output, y[:, i])
-
-        # loss = loss / max_seq_len
-
-        # return {"loss": loss}
+        # # context_v
 
     def prediction(self, batch, batch_idx):
         x, y = batch
--- src/seq2seq/dataset/math.py
+++ src/seq2seq/dataset/math.py
@@ -1,8 +1,8 @@
+import numpy as np
 import random
-
 import inflect
 import pandas as pd
-
+from sklearn.model_selection import train_test_split
 
 class MathDatasetGenerator:
     digits = list("0123456789")
@@ -12,18 +12,18 @@ class MathDatasetGenerator:
     def __init__(self):
         self._inflect_engine = inflect.engine()
 
-    def _generate_number(self, max_digits=3):
-        length = random.randint(1, max_digits)
+    def _generate_number(self, max_digits=4):
+        length = np.random.randint(1, max_digits+1)
         number = ""
         for _ in range(length):
-            number += str(random.randint(0, 9))
+            number += str(np.random.randint(0, 10))
 
         text = self._inflect_engine.number_to_words(int(number))
 
         return number, text
 
     def _generate_operator(self):
-        op_idx = random.randint(0, len(self.operators) - 1)
+        op_idx = np.random.randint(0, len(self.operators))
         return (self.operators[op_idx], self.operator_names[op_idx])
 
     def _generate_expression(self, max_length):
@@ -47,12 +47,22 @@ class MathDatasetGenerator:
     def generate_expressions(self, size: int):
         rows = []
         for _ in range(size):
-            rows.append(self._generate_expression(3))
+            rows.append(self._generate_expression(5))
 
         df = pd.DataFrame(data=rows, columns=["numbers", "text"])
+        df = df.drop_duplicates()
+
+
+        df_test, df_keep = train_test_split(df, train_size=0.2, shuffle=True)
+        df_train, df_dev = train_test_split(df_keep, train_size=0.8, shuffle=True)
+
+        df_train.to_csv('data/raw/math.train')
+        df_dev.to_csv('data/raw/math.dev')
+        df_test.to_csv('data/raw/math.test')
+
         df.to_csv("data/raw/math.csv")
 
 
 if __name__ == "__main__":
     mdg = MathDatasetGenerator()
-    mdg.generate_expressions(1000)
+    mdg.generate_expressions(50_000)
