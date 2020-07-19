e39f66c3321279abf86d21d353869608c372ec6c
Mateusz Bednarski <mateusz.bednarski@windowslive.com>
2020-07-19 13:05:55 +0200

    Testing batch size=1 for attention

--- tests/test_attention.py
+++ tests/test_attention.py
@@ -1,5 +1,4 @@
 import torch
-from torch import bartlett_window
 
 from seq2seq.attention import Attention
 
@@ -8,6 +7,24 @@ def test_attention():
     encoder_hidden_size = 20
     decoder_hidden_size = 30
     batch_size = 64
+    src_seq_len = 50
+
+    attn = Attention(encoder_hidden_size, decoder_hidden_size)
+
+    decoder_hidden = torch.rand([batch_size, decoder_hidden_size])
+    encoder_outputs = torch.rand([batch_size, src_seq_len, encoder_hidden_size])
+
+    a = attn.forward(decoder_hidden, encoder_outputs)
+
+    assert a.shape == (batch_size, src_seq_len)
+    print(type(a.sum(dim=1)))
+    assert torch.allclose(a.sum(dim=1), torch.ones(batch_size))
+
+
+def test_attention_for_batch_equal_one():
+    encoder_hidden_size = 20
+    decoder_hidden_size = 30
+    batch_size = 1
     src_seq_len = 50
 
     attn = Attention(encoder_hidden_size, decoder_hidden_size)
