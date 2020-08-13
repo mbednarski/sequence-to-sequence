f70608cd91d05720c848564c2806695ebf036b24
Mateusz Bednarski <mateusz.bednarski@roche.com>
2020-08-13 12:43:43 +0200

    Fixed module imports

--- src/seq2seq/__init__.py
+++ src/seq2seq/__init__.py
@@ -1,3 +1,3 @@
-from .attention import Attention
-from .decoder import Decoder
-from .encoder import Encoder
+from seq2seq.attention import Attention
+from seq2seq.decoder import Decoder
+from seq2seq.encoder import Encoder
