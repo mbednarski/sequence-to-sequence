16413ec2425b95f19c39fd5471b35cb863ff2550
Mateusz Bednarski <mateusz.bednarski@windowslive.com>
2020-08-15 19:49:56 +0200

    BPE vocab building

---
+++ bpe.py
@@ -0,0 +1,15 @@
+corpus = "the thesaurus speedy and greedy and fast for the us"
+
+# simple tokenizer: the thesaurus speedy greedy and fast for us
+# V = 8
+
+{
+    "the": 2,
+    "thesaurus": 1,
+    "speedy": 1,
+    "and": 2,
+    "greedy": 1,
+    "fast": 1,
+    "for": 1,
+    "us": 1,
+}
